6bfc5cee03c25ac6a27f904ed2369e61dfe830ad
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-03 03:21:33 +0000

    style added to dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
-  @categories_count = Category.all.count
   @categories = Category.all
-
-  @each_category_products_count = @categories.map {
-    |item|
-   item.products.count
-  }
-  @products_total_count = Product.all.count
+  @products = @categories.product
+  @product_total = Product.all.count
   end
 end
