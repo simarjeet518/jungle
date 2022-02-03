c60de3844510edc9cbc78e1ce82892db5e2e97cb
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-03 01:53:12 +0000

    show controller instance variable update to each product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
   def show
+  @category_count = Category.all.count
+  @category = Category.all
+
+  @Each_category_product_count = @category.map {
+    |item|
+   item.products.count
+  }
+  @product_total_count = Product.all.count
   end
 end
