8b28219bc4bdf8f8a163e88afbfd271aaf1999c8
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-03 21:04:08 +0000

    new and create func added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,5 +3,26 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
   
 end
