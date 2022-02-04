592c12271a31ffa66771739c23b920289aa08a54
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-04 03:20:20 +0000

    validation added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password,:length => {:within => 6..40}
+ 
+  def check_confirm_password
+    if password_confirmation != password
+      errors.add(:password, "confrimation failed") 
+    end
+  end
 end
