d6987b921b66eb68928bc3016fa762771a699041
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-04 04:42:12 +0000

    error display on login failed fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    
+    @error = false
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id 
       # only asking authorization when user trying to checkout then after login redirected to same view
       redirect_to '/cart'
     else
-      redirect_to '/login'
+      @error = true
+      render :new
     end
   end
 
