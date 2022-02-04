5fcc9fe63f9692ca19e372baa9bdbe9ef6071c39
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-04 03:21:05 +0000

    user and session routes controller finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
   def new
+  @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render :new
     end
   end
 
