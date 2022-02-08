fcd8ce3812670505a869b241da037e67b165f467
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-02-08 22:32:49 +0000

    first auntomated test for product page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,20 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist" # Add this line to require poltergeist
-
-# Specs flagged with `js: true` will use Capybara's JS driver. Set
-# that JS driver to :poltergeist
-Capybara.javascript_driver = :poltergeist
-ENV['RAILS_ENV'] ||= 'test'
-
+ # Add this line to require poltergeist
+ require "spec_helper"
+ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist"
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
